8340b87b5adc2b68cf100675de4075fb28c08813
LinuxMistery00 <eduardosbarros.ed@gmail.com>
2024-10-04 13:04:54 -0300

    Header

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="bg-zinc-950 h-20">
+            <h1 className="text-white m-4 fixed text-5xl font-mono">Biznesi</h1>
+        </div>
+    )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Header from './components/Header';
 import Products from './components/Products'
 import "./styles/global.scss"
 
 export default function Home() {
   return (
     <div>
-      
+      <Header/>
     </div>
   );
 }
