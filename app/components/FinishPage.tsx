25f59a76850f60680bbb113e9951376d9a8be96d
LinuxMistery00 <eduardosbarros.ed@gmail.com>
2024-10-11 04:25:14 -0300

    Redirect to /

--- app/components/FinishPage.tsx
+++ app/components/FinishPage.tsx
@@ -1,27 +1,29 @@
 "use client";
 import React from 'react';
 import { useTotalPrice } from '../components/TotalPriceContext';
+import Link from 'next/link';
 
 const FinishPage: React.FC = () => {
   const { totalPrice, resetTotal } = useTotalPrice();
 
   const handleFinish = () => {
-    alert(`Compra finalizada com sucesso! Total pago: $${totalPrice.toFixed(2)}`);
-    
     resetTotal();
+
+
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">Resumo da Compra</h1>
-      <p className="text-lg mb-8">Total a pagar: <span className="font-semibold">${totalPrice.toFixed(2)}</span></p>
+      <h1 className="text-2xl font-bold mb-4">Purchase Summary</h1>
+      <p className="text-lg mb-8">Total payable: <span className="font-semibold">${totalPrice.toFixed(2)}</span></p>
       
+      <Link href="/">
       <button
         onClick={handleFinish}
-        className="px-6 py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-300"
-      >
-        Finalizar Compra
+        className="px-6 py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-300">
+        Finalize Purchase
       </button>
+      </Link>
     </div>
   );
 };
